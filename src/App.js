7c20e6634754fe8446fd4e5b62ca0597c7a31db5
ijTuhin <ijTuhin638@gmail.com>
2022-04-17 12:00:04 -0700

    react router installed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Footer from './Component/Footer/Footer';
+import Header from './Component/Header/Header';
+import Home from './Component/Home/Home';
+import Login from './Component/Login/Login';
 
 function App() {
   return (
-    <div className="App">
-      <h3>Start</h3>
+    <div>
+      <Header></Header>
+      <Routes>
+        <Route path='/'>{<Home></Home>}</Route>
+        <Route path='/login'>{<Login></Login>}</Route>
+      </Routes>
+      <Footer></Footer>
     </div>
   );
 }
