d9513514a3829508af5e7fc605c9e99824ae15c5
ijTuhin <ijTuhin638@gmail.com>
2022-04-17 15:45:52 -0700

    handle input field onBlur

--- src/Component/SignUp/SignUp.js
+++ src/Component/SignUp/SignUp.js
@@ -1,32 +1,51 @@
 import React, { useState } from 'react';
 import './SignUp.css';
+
 const SignUp = () => {
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    const [] = useState('');
+    const [error, setError] = useState('');
+    const [registered, setRegistered] = useState(false);
+    const [validation, setvalidation] = useState(false);
+
+
+
+    const handleUserNameBlur = event => {
+        setName(event.target.value);
+    }
+    const handleUserEmailBlur = event => {
+        setEmail(event.target.value);
+    }
+    const handleUserPasswordBlur = event => {
+        setPassword(event.target.value);
+    }
+
+
+
+
     return (
         <div className='d-flex justify-content-center my-5'>
             <div className="form-container">
                 <h3>Please <span>Sign-up</span></h3>
                 <form >
                     <div className='input-field'>
-                        <input type="text" name="name" placeholder='Enter name' id="" />
+                        <input onBlur={handleUserNameBlur} type="text" name="name" placeholder='Enter name' />
                     </div>
                     <div className='input-field'>
-                        <input type="email" name="email" placeholder='Enter email' id="" />
+                        <input onBlur={handleUserEmailBlur} type="email" name="email" placeholder='Enter email' />
                     </div>
-                    <div >
-                        <input type="password" name="password" placeholder="Enter password" />
+                    <div>
+                        <input onBlur={handleUserPasswordBlur} type="password" name="password" placeholder="Enter password" />
                         <input type="password" name="confirm-password" placeholder="Confirm password" />
                     </div>
-                    <div >
-                        <input type="tel" name="phone-number" placeholder='Phone Number' id="" />
-                        <input type="text" name="address" placeholder='Enter address' id="" />
+                    <div>
+                        <input type="tel" name="phone-number" placeholder='Phone Number' />
+                        <input type="text" name="address" placeholder='Enter address' />
                     </div>
                     <div className='submit-field'>
-                    <input type="checkbox" name="" id="" /> <span id='form-q'>Already a user?</span><br />
-                    <button className='border-0 px-2 py-1 rounded' type="submit">Sign Up</button>
+                        <input  type="checkbox" name="" /> <span id='form-q'>Already a user?</span><br />
+                        <button className='border-0 px-2 py-1 rounded' type="submit">Sign Up</button>
                     </div>
                 </form>
             </div>
