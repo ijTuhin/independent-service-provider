850f67f4a46e4cd5fc672f8b2d4cf375f0406b25
ijTuhin <ijTuhin638@gmail.com>
2022-04-17 17:10:09 -0700

    forgot password btn for login form

--- src/Component/SignUp/SignUp.js
+++ src/Component/SignUp/SignUp.js
@@ -55,24 +55,54 @@ const SignUp = () => {
             <div className="form-container">
                 <h3>Please <span>{registered ? 'Login' : 'Sign-up'}</span></h3>
                 <form noValidate validated={validated} onSubmit={handleFormSubmit}>
-                    <div className='input-field'>
-                        <input onBlur={handleUserNameBlur} type="text" name="name" placeholder='Enter name' />
-                    </div>
+                    {
+                        registered ?
+                            <></>
+                            :
+                            <>
+                                <div className='input-field'>
+                                    <input onBlur={handleUserNameBlur} type="text" name="name" placeholder='Enter name' />
+                                </div>
+                            </>
+                    }
                     <div className='input-field'>
                         <input onBlur={handleUserEmailBlur} type="email" name="email" placeholder='Enter email' />
                     </div>
-                    <div>
-                        <input onBlur={handleUserPasswordBlur} type="password" name="password" placeholder="Enter password" />
-                        <input type="password" name="confirm-password" placeholder="Confirm password" />
-                    </div>
-                    <div>
-                        <input type="tel" name="phone-number" placeholder='Phone Number' />
-                        <input type="text" name="address" placeholder='Enter address' />
-                    </div>
+                    {
+                        registered ?
+                            <>
+                                <div className='input-field'>
+                                    <input onBlur={handleUserPasswordBlur} type="password" name="password" placeholder="Enter password" />
+                                </div>
+                            </>
+                            :
+                            <>
+                                <div>
+                                    <input onBlur={handleUserPasswordBlur} type="password" name="password" placeholder="Enter password" />
+                                    <input type="password" name="confirm-password" placeholder="Confirm password" />
+                                </div>
+                                <div>
+                                    <input type="tel" name="phone-number" placeholder='Phone Number' />
+                                    <input type="text" name="address" placeholder='Enter address' />
+                                </div>
+                            </>
+                    }
                     <div className='submit-field'>
-                        <input onChange={handleUserRegisterdStateChange} type="checkbox" name="" /> <span id='form-q'>Already a user?</span><br />
-                        <button className='border-0 px-2 py-1 rounded' type="submit">{registered ? 'Login' : 'Sign Up'}</button>
+
+                        <div className='d-flex justify-content-between align-items-center'>
+                            <div>
+                                <input onChange={handleUserRegisterdStateChange} type="checkbox" name="" /> <span id='form-q'>Already a user?</span>
+                            </div>
+                            {registered
+                                ?
+                                <><button className='forgot-btn'>Forgot password?</button></>
+                                :
+                                <></>
+                            }
+                        </div>
+                        <button className='border-0 px-2 py-1 rounded submit-btn' type="submit">{registered ? 'Login' : 'Sign Up'}</button>
                     </div>
+
                 </form>
             </div>
         </div>
