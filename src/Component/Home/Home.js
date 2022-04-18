7e78b511da8a5bee29dccb3d5691eaa9c8e01998
ijTuhin <ijTuhin638@gmail.com>
2022-04-18 15:59:56 -0700

    fixed carousel size

--- src/Component/Home/Home.js
+++ src/Component/Home/Home.js
@@ -23,15 +23,17 @@ const Home = () => {
                     <div class="carousel-item active">
                         <img src={banner1} class="d-block w-100" alt="..." />
                         <div class="carousel-caption d-none d-md-block">
-                            <h5>Service Provider</h5>
-                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. <br /> Blanditiis rem nihil odit placeat cumque voluptates ipsam <br /> assumenda distinctio nobis fuga quibusdam odio ut sequi, <br /> reprehenderit dignissimos magni exercitationem dolore quis delectus <br /> amet vero sed dolorem porro? Eum animi illum hic ullam harum, <br /> nemo incidunt nesciunt asperiores autem quibusdam reiciendis modi.Lorem ipsum dolor <br /> sit amet consectetur adipisicing elit. Dolore unde commodi dignissimos. Error, optio? Cupiditate, <br /> fuga placeat non ducimus, quod sequi in quos cumque tempora, distinctio nulla vel et expedita?<br /> Aut odio ullam veniam fuga placeat, veritatis ab cumque quas nostrum provident <br /> nobis tenetur dicta labore quis laudantium sed quidem atque illum quibusdam consectetur. <br /> Incidunt fugiat, dignissimos harum praesentium error iusto? </p>
+                            <h1 className='text-secondary fw-bold fst-italic fs-2'>Foodie Food</h1>
+                            <small className='text-secondary fw-bold fst-italic fs-5'>Catering Service</small>
+                            <p>Serve your tummy with our yummy food. <br /> Order for your parties, special days, or regular need.</p>
                         </div>
                     </div>
                     <div class="carousel-item">
                         <img src={banner2} class="d-block w-100" alt="..." />
                         <div class="carousel-caption d-none d-md-block">
-                            <h5>Service Provider</h5>
-                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. <br /> Blanditiis rem nihil odit placeat cumque voluptates ipsam <br /> assumenda distinctio nobis fuga quibusdam odio ut sequi, <br /> reprehenderit dignissimos magni exercitationem dolore quis delectus <br /> amet vero sed dolorem porro? Eum animi illum hic ullam harum, <br /> nemo incidunt nesciunt asperiores autem quibusdam reiciendis modi.Lorem ipsum dolor <br /> sit amet consectetur adipisicing elit. Dolore unde commodi dignissimos. Error, optio? Cupiditate, <br /> fuga placeat non ducimus, quod sequi in quos cumque tempora, distinctio nulla vel et expedita?<br /> Aut odio ullam veniam fuga placeat, veritatis ab cumque quas nostrum provident <br /> nobis tenetur dicta labore quis laudantium sed quidem atque illum quibusdam consectetur. <br /> Incidunt fugiat, dignissimos harum praesentium error iusto? </p>
+                            <h1 className='text-light fw-bold fst-italic fs-2'>Foodie Food</h1>
+                            <small className='text-light fw-bold fst-italic fs-5'>Catering Service</small>
+                            <p>Serve your tummy with our yummy food. <br /> Order for your parties, special days, or regular need. </p>
                         </div>
                     </div>
                 </div>
