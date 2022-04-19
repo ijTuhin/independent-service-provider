2d0b6dc3ae512ee55fe9ce791c73762375bfad87
ijTuhin <ijTuhin638@gmail.com>
2022-04-18 23:54:18 -0700

    footer section responsive

--- src/Component/Footer/Footer.js
+++ src/Component/Footer/Footer.js
@@ -4,35 +4,28 @@ import './Footer.css';
 const Footer = () => {
     return (
         <div className='footer'>
-            <div class="box-container">
+            <div class="box-container pt-5">
+                <div class="box d-flex justify-content-center align-items-center">
+                    <div class="d-flex justify-content-center align-items-center">
+                        <Link to="/"> <i class="ms-2 text-warning fas fa-angle-right"></i> Home</Link>
+                        <Link to="/about"> <i class="ms-2 text-warning fas fa-angle-right"></i> About</Link>
+                        <Link to="/blogs"> <i class="ms-2 text-warning fas fa-angle-right"></i> Blogs</Link>
+                        <Link to="/"> <i class="ms-2 text-warning fas fa-angle-right"></i> Services</Link>
+                    </div>
+                </div>
+                <div class="text-white text-center pt-4">
+                    <p> Catering <span>Service</span> || 2022 © all rights
+                        reserved! </p>
+                </div>
                 <div class="box">
-                    <h3>Quick links</h3>
-                    <Link to="/"> <i class="text-warning fas fa-angle-right"></i> Home</Link>
-                    <Link to="/about"> <i class="text-warning fas fa-angle-right"></i> About</Link>
-                    <Link to="/blogs"> <i class="text-warning fas fa-angle-right"></i> Blogs</Link>
-                    <Link to="/"> <i class="text-warning fas fa-angle-right"></i> Services</Link>
+                    <div class="d-flex justify-content-center align-items-center">
+                        <a href="/"> <i class="text-warning fab fa-facebook-f"></i></a>
+                        <a href="/"> <i class="text-warning fab fa-twitter"></i></a>
+                        <a href="/"> <i class="text-warning fab fa-instagram"></i></a>
+                        <a href="/"> <i class="text-warning fab fa-linkedin"></i></a>
+                    </div>
                 </div>
 
-                <div class="box">
-                    <h3>Contact info</h3>
-                    <a href="/"> <i class="text-warning fas fa-phone"></i> +880-111-222-3333 </a>
-                    <a href="/"> <i class="text-warning fas fa-envelope"></i> abcd1234@gmail.com </a>
-                    <a href="/"> <i class="text-warning fas fa-map"></i> Chattagram, Bangladesh </a>
-                </div>
-
-                <div class="box">
-                    <h3>Follow us</h3>
-                    <a href="/"> <i class="text-warning fab fa-facebook-f"></i> Facebook </a>
-                    <a href="/"> <i class="text-warning fab fa-twitter"></i> Twitter </a>
-                    <a href="/"> <i class="text-warning fab fa-instagram"></i> Instagram </a>
-                    <a href="/"> <i class="text-warning fab fa-linkedin"></i> Linkedin </a>
-                </div>
-
-            </div>
-
-            <div class="text-white text-center pb-4">
-                <p> Catering <span>Service</span> || 2022 © all rights
-                reserved! </p>
             </div>
         </div>
     );
